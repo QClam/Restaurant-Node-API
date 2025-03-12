6aaa9390386b2f4fb140980cc061b00ab197638f
Xuan Quy <hoangxuanquy5@gmail.com>
2025-03-12 10:41:33 +0700

    Deploy API

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-    console.log(`Server is running on Server is running at http://localhost:3000`)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Server is running at http://localhost:${PORT}`)
 })
 
 app.get('/', (req, res) => {
